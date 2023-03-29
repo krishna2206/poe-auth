cb6a893caf198f0a653f5b4f995d88d51c60b84e
Fitiavana Krishna <fitiavana.krishna@gmail.com>
2023-03-29 17:19:01 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=['requests', 'beautifulsoup4', 'click'],
     entry_points={
         'console_scripts': [
-            'poe-auth=poe_auth.poe_auth',
+            'poe-auth=poe_auth.poe_auth:cli',
         ],
     },
     classifiers=[
